3986b745b1527d2d91c8cd92cc6c48cdeb5b446e
Xiao Zhong <zhongxiao2017@gmail.com>
2023-10-28 17:56:52 +1100

    fix date default value

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,13 +8,23 @@ const index = async (req, res) => {
 }
 
 const newFlight = (req, res) => {
+  // Create a new Flight object with default values
+  const newFlight = new flightsDb();
+  // Format the default date for the input field
+  const dt = newFlight.departs;
+  let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+  departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+  console.log('date',departsDate);
+ 
 
-  res.render('flights/new',{errorMsg:''})
+  res.render('flights/new', { errorMsg: '', departsDate });
 }
 
 
 
+
 const create = async (req, res) => {
+ const{arline, airport,flightNo,departs}=req.body
   try {
     await flightsDb.create(req.body);
  res.redirect('/flights'); }
@@ -26,6 +36,7 @@ const create = async (req, res) => {
  
   
 }
+
 
 
 module.exports = {
